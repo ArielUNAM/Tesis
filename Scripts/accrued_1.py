9979974e86e33028c0202b1aa9bddebdb3f31221
arielcg <arielcerong@gmail.com>
2020-01-27 12:41:15 -0600

    Update accrued_1.py
    Añadimos filtros para quitar el ruido, aún en construcción

--- Scripts/accrued_1.py
+++ Scripts/accrued_1.py
@@ -16,6 +16,7 @@
 import glob
 import os
 import wradlib as wrl
+from scipy import stats
 import warnings
 import matplotlib.pyplot as pl
 import numpy as np
@@ -43,13 +44,13 @@ os.chdir(path)
 print("Directory of data:", os.getcwd())
 
 #Definimos las fechas para la lectura de datos
-dateS = date(2015, 3, 6)    #Start date
+dateS = date(2015, 3, 9)    #Start date
 dateE = date(2015, 10, 30)     #End date
 delta = dateE - dateS
 
 #Definimos el nombre de los archivos segun la fecha definida
-#filename = "/RAW_NA_000_236_2015"+'%02d'%dateS.month+'%02d'%dateS.day+"*"
-filename = "/RAW_NA_000_236_2015"+'%02d'%dateS.month+"*"
+filename = "/RAW_NA_000_236_2015"+'%02d'%dateS.month+'%02d'%dateS.day+"*"
+#filename = "/RAW_NA_000_236_2015"+'%02d'%dateS.month+"*"
 #filename = "/RAW_NA_000_236_2015"+"*"
 
 allFiles = sorted(glob.glob(path+filename))
@@ -62,11 +63,11 @@ dataMatriz = np.zeros((360,921))
 i = 0   #Contador
 j = 0
 
-numberofE = 10
+numberofE = 50
 #Iniciamos el ciclo que recogera el acumulado
-for fname in allFiles:
-#for index in range(numberofE):
-    #fname = allFiles[index]
+#for fname in allFiles:
+for index in range(numberofE):
+    fname = allFiles[index]
     #print(fname)
     j += 1
     f = wrl.util.get_wradlib_data_file(fname)   #Set the name 
@@ -84,8 +85,8 @@ for fname in allFiles:
     #print("Max value: ",max(range_rad),"Min value: ",min(range_rad))
     #print(range_rad)
     #print(range_rad[-1],range_rad.shape[0])
-    print("Restantes: ",len(allFiles)-j)
-    #print("Restantes: ",numberofE - index)
+    #print("Restantes: ",len(allFiles)-j)
+    print("Restantes: ",numberofE - index)
     #Cuando el valor de exploración es 236000 es porque hubo lluvia, que es el momento que nos interesa
     if range_rad[-1] == 236000.0 and range_rad.shape[0] == 921:
         if fcontent['product_hdr']['product_configuration']['product_specific_info']['sweep_number'] == 1:
@@ -99,6 +100,12 @@ for fname in allFiles:
             print("Date:",año,"-",mes,"-",dia,"\nTime:",hora,":",minuto)
             range_radc_bien = range_rad
             d = fcontent['data'][1]['sweep_data']['DB_DBZ']['data']
+            b = fcontent['data'][1]['sweep_data']['DB_VEL']['data']
+            print("Maximo: ", np.ma.max(b), "Minimo: ", np.ma.min(b))
+
+            #Eliminamos los elementos que no se mueven
+            d[b == 0] = 0
+            d[b.mask == True] = 0
             #Limpiamos y filtramos la información
             clutter = wrl.clutter.filter_gabella(d, tr1=12, n_p=6, tr2=1.1)
             data_no_clutter = wrl.ipol.interpolate_polar(d, clutter)
@@ -127,7 +134,7 @@ for fname in allFiles:
             pl.grid(color="grey")
             ii = i  
             ii = str(ii)
-            pl.savefig(pathImg+"/"+ii+'.png')
+            pl.savefig(pathImg+"/"+ii+'b.png')
             print('Imagen guardada')
             i += 1
             pl.close()
@@ -138,10 +145,10 @@ cmap.set_bad(color='white')
 ax, cf = wrl.vis.plot_ppi(dataMatriz , cmap="viridis")
 pl.xlabel("Easting from radar (km)")
 pl.ylabel("Northing from radar (km)")
-#pl.title("Radar Queretaro"+ u'Precipitación (mm),'+ año +"-"+mes+"-"+dia+" "+hora+":"+minuto)
+pl.title("Radar Queretaro"+ u'Precipitación (mm),'+ año +"-"+mes+"-"+dia)
 cb = pl.colorbar(cf, shrink=0.8)
 cb.set_label("mm")
 pl.xlim(-300,300)
 pl.ylim(-300,300)
 pl.grid(color="grey")
-pl.savefig(path+'/acumulacion'+'.png')       
+pl.savefig(pathImg+'/acumulacion'+'b.png')       
