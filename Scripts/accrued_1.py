55174c9d28d5d569caacca2050fac884672a7d41
arielcg <arielcerong@gmail.com>
2020-02-13 13:03:21 -0600

    Lunes 10
    Modificación al script para hacer el acumulado
    Retire filtros de enmascarado

--- Scripts/accrued_1.py
+++ Scripts/accrued_1.py
@@ -20,6 +20,7 @@ import glob
 import os
 import warnings
 from datetime import date, timedelta
+import pandas as pd
 
 import matplotlib.pyplot as pl
 import numpy as np
@@ -60,14 +61,21 @@ año = str(dateS.year)
 mes = str(dateS.month)
 dia = str(dateS.day)
 
-#Definimos el nombre de los archivos según la fecha definida
 
+#Para saber cuantos meses hay en la carpeta de datos 
+#allFileNames = "/RAW_NA_000_"+"*"
+#allFilenames = sorted(glob.glob(path+allFileNames))
+#Fisrtmonth = allFileNames[0][77:79]
+#Lastmonnth = allFileNames[-1][77:79]
+
+
+#Definimos el nombre de los archivos según la fecha definida
 #Fecha usando el año
 #filename = "/RAW_NA_000_236_2015"+'%04d'%dateS.year+"*"
 #Fecha usando el mes
-#filename = "/RAW_NA_000_236_2015"+'%02d'%dateS.month+"*"
+filename = "/RAW_NA_000_236_2015"+'%02d'%dateS.month+"*"
 #Fecha usando el día
-filename = "/RAW_NA_000_236_"+'%04d'%dateS.year+'%02d'%dateS.month+'%02d'%dateS.day+"*"
+#filename = "/RAW_NA_000_236_"+'%04d'%dateS.year+'%02d'%dateS.month+'%02d'%dateS.day+"*"
 #Sin uso de fecha
 #filename = "/RAW_NA_000_236_2015"+"*"
 
@@ -82,12 +90,12 @@ i = 0   #Contador para enumerar las imagenes
 j = 0   #Contador para saber la cantidad de datos procesados
 
 #Variables para depurar el programa
-numberofE = 50
+#numberofE = 50
 #Iniciamos el ciclo que recogerá el acumulado
-#for fname in allFiles:
-for index in range(numberofE):
+for fname in allFiles:
+#for index in range(numberofE):
     #Recorremos cada nombre almacenado en allFiles
-    fname = allFiles[index]
+    #fname = allFiles[index]
     #print(fname)
     j += 1  #Aumento en el contador de datos procesados
     
@@ -99,90 +107,94 @@ for index in range(numberofE):
     #de un archivo VS, a la salida de la lectura obtenemos un diccionario que
     #contiene todos los datos contenidos usualmente en este tipo de archivos
     #Un nivel abajo de la carpeta guardo el archivo que contiene información de Vaisal
-    fcontent = wrl.io.read_iris(f)  
-    #print(fcontent.keys())
-    #print the entire content including values of data and
-    #metadata of the first sweep
-    #(numpy arrays will not be entirely printed)
-    #print(fcontent['data'][1])
-    nbins = fcontent['product_hdr']['product_end']['number_bins']
-    #print(nbins)
-    gate_0 = fcontent['ingest_header']['task_configuration']['task_range_info']['range_first_bin']/100
-    #print(gate_0)
-    gate_nbin = fcontent['ingest_header']['task_configuration']['task_range_info']['range_last_bin']/100
-    gate_size = round((gate_nbin - gate_0)/(nbins))
-    range_rad = gate_0 + gate_size * np.arange(nbins,dtype='float32')
-    #Cuando el valor de exploración es 236000 es porque hubo lluvia, que es el momento que nos interesa
-    #print("Max value: ",max(range_rad),"Min value: ",min(range_rad))
-    #print(range_rad)
-    #print(range_rad[-1],range_rad.shape[0])
-    #print("Restantes: ",len(allFiles)-j)
-    print("Restantes: ",numberofE - index)
-    #Cuando el valor de exploración es 236000 es porque hubo lluvia, que es el momento que nos interesa
-    if range_rad[-1] == 236000.0 and range_rad.shape[0] == 921:
-        if fcontent['product_hdr']['product_configuration']['product_specific_info']['sweep_number'] == 1:
-            #Datos de la gfecha según su etiqueta en el nombre
-            #Se puede mejorar la identificación de la hora si se restra el tamaño del path menos el tamaño total de fname y los quince primeros elementos del nombre
-            año = fname[73:77]  
-            mes = fname[77:79]
-            dia = fname[79:81]
-            hora = fname[81:83]
-            minuto = fname[83:85]
-            print("Date:",año,"-",mes,"-",dia,"\nTime:",hora,":",minuto)
-            range_radc_bien = range_rad
-            d = fcontent['data'][1]['sweep_data']['DB_DBZ']['data']
-            b = fcontent['data'][1]['sweep_data']['DB_VEL']['data']
-            print("Maximo: ", np.ma.max(b), "Minimo: ", np.ma.min(b))
+    try:
+        fcontent = wrl.io.read_iris(f)  
+        #print(fcontent.keys())
+        #print the entire content including values of data and
+        #metadata of the first sweep
+        #(numpy arrays will not be entirely printed)
+        #print(fcontent['data'][1])
+        nbins = fcontent['product_hdr']['product_end']['number_bins']
+        #print(nbins)
+        gate_0 = fcontent['ingest_header']['task_configuration']['task_range_info']['range_first_bin']/100
+        #print(gate_0)
+        gate_nbin = fcontent['ingest_header']['task_configuration']['task_range_info']['range_last_bin']/100
+        gate_size = round((gate_nbin - gate_0)/(nbins))
+        range_rad = gate_0 + gate_size * np.arange(nbins,dtype='float32')
+        #Cuando el valor de exploración es 236000 es porque hubo lluvia, que es el momento que nos interesa
+        #print("Max value: ",max(range_rad),"Min value: ",min(range_rad))
+        #print(range_rad)
+        #print(range_rad[-1],range_rad.shape[0])
+        print("Restantes: ",len(allFiles)-j)
+        #print("Restantes: ",numberofE - index)
+        #Cuando el valor de exploración es 236000 es porque hubo lluvia, que es el momento que nos interesa
+        if range_rad[-1] == 236000.0 and range_rad.shape[0] == 921:
+            if fcontent['product_hdr']['product_configuration']['product_specific_info']['sweep_number'] == 1:
+                #Datos de la gfecha según su etiqueta en el nombre
+                #Se puede mejorar la identificación de la hora si se restra el tamaño del path menos el tamaño total de fname y los quince primeros elementos del nombre
+                año = fname[73:77]  
+                mes = fname[77:79]
+                dia = fname[79:81]
+                hora = fname[81:83]
+                minuto = fname[83:85]
+                print("Date:",año,"-",mes,"-",dia,"\nTime:",hora,":",minuto)
+                range_radc_bien = range_rad
+                d = fcontent['data'][1]['sweep_data']['DB_DBZ']['data']
+                b = fcontent['data'][1]['sweep_data']['DB_VEL']['data']
+                #print("Maximo: ", np.ma.max(b), "Minimo: ", np.ma.min(b))
 
-            #Eliminamos los elementos que no se mueven
-            #d[b == 0] = 0
-            d[b.mask == True] = 0
-            #Limpiamos y filtramos la información
-            clutter = wrl.clutter.filter_gabella(d, tr1=12, n_p=6, tr2=1.1)
-            data_no_clutter = wrl.ipol.interpolate_polar(d, clutter)
-            #pia = wrl.atten.correctAttenuationKraemer(data_no_clutter)
-            # pia = wrl.atten.constraint_pia(data_no_clutter)
-            pia = wrl.atten.correct_attenuation_constrained(data_no_clutter, a_max=1.67e-4,
-                a_min=2.33e-5,
-                n_a=100,
-                b_max=0.7,
-                b_min=0.65,
-                n_b=6,
-                gate_length=0.25,
-                constraints=[wrl.atten.constraint_dbz,
-                 wrl.atten.constraint_pia],
-                constraint_args=[[59.0],[20.0]])
-            data_attcorr = data_no_clutter + pia
-            #data_attcorr = data_no_clutter
-            Z = wrl.trafo.idecibel(data_attcorr) 
-            #R = wrl.zr.z2r(Z, a=220., b=2.0)
-            R = wrl.zr.z_to_r(Z,a=220.00,b=1.6)
-            #depth = wrl.trafo.r2depth(R, 390)
-            depth = wrl.trafo.r_to_depth(R,390)
-            Zc = depth*4
-            print("ZC max:", np.max(Zc))
-            dataMatriz += Zc
-            print("DM max: ",np.max(dataMatriz))
-            #Zc = np.ma.masked_where(Zc < 0.05, Zc)
-            cmap = pl.cm.viridis
-            cmap.set_bad(color='white')
-            ax, cf = wrl.vis.plot_ppi(Zc, cmap="viridis")
-            pl.xlabel("Easting from radar (km)")
-            pl.ylabel("Northing from radar (km)")
-            pl.title("Radar Queretaro"+ u'Precipitación (mm),'+año+"-"+mes+"-"+dia+" "+hora+":"+minuto)
-            cb = pl.colorbar(cf, shrink=0.8)
-            cb.set_label("mm")
-            pl.xlim(-300,300)
-            pl.ylim(-300,300)
-            pl.grid(color="grey")
-            ii = i  
-            ii = str(ii)
-            pl.savefig(pathImg+"/"+ii+'b.png')
-            print('Imagen guardada')
-            i += 1
-            pl.close()
+                #Eliminamos los elementos que no se mueven
+                #d[b == 0] = 0
+                d[b.mask == True] = 0
+                #Limpiamos y filtramos la información
+                clutter = wrl.clutter.filter_gabella(d, tr1=12, n_p=6, tr2=1.1)
+                data_no_clutter = wrl.ipol.interpolate_polar(d, clutter)
+                #pia = wrl.atten.correctAttenuationKraemer(data_no_clutter)
+                # pia = wrl.atten.constraint_pia(data_no_clutter)
+                pia = wrl.atten.correct_attenuation_constrained(data_no_clutter, a_max=1.67e-4,
+                    a_min=2.33e-5,
+                    n_a=100,
+                    b_max=0.7,
+                    b_min=0.65,
+                    n_b=6,
+                    gate_length=0.25,
+                    constraints=[wrl.atten.constraint_dbz,
+                    wrl.atten.constraint_pia],
+                    constraint_args=[[59.0],[20.0]])
+                data_attcorr = data_no_clutter + pia
+                #data_attcorr = data_no_clutter
+                Z = wrl.trafo.idecibel(data_attcorr) 
+                #R = wrl.zr.z2r(Z, a=220., b=2.0)
+                R = wrl.zr.z_to_r(Z,a=220.00,b=1.6)
+                #depth = wrl.trafo.r2depth(R, 390)
+                depth = wrl.trafo.r_to_depth(R,390)
+                Zc = depth*4
+                #print("ZC max:", np.max(Zc))
+                dataMatriz += Zc
+                #print("DM max: ",np.max(dataMatriz))
+                #Zc = np.ma.masked_where(Zc < 0.05, Zc)
+                #cmap = pl.cm.viridis
+                #cmap.set_bad(color='white')
+                #ax, cf = wrl.vis.plot_ppi(Zc, cmap="viridis")
+                #pl.xlabel("Easting from radar (km)")
+                #pl.ylabel("Northing from radar (km)")
+                #pl.title("Radar Queretaro"+ u'Precipitación (mm),'+año+"-"+mes+"-"+dia+" "+hora+":"+minuto)
+                #cb = pl.colorbar(cf, shrink=0.8)
+                #cb.set_label("mm")
+                #pl.xlim(-300,300)
+                #pl.ylim(-300,300)
+                #pl.grid(color="grey")
+                #ii = i  
+                #ii = str(ii)
+                #pl.savefig(pathImg+"/"+ii+'b.png')
+                #print('Imagen guardada')
+                #i += 1
+                #pl.close()
+    except:
+        print("File",fname,"corrupt")
 
 #dataMatriz = np.ma.masked_where(dataMatriz < 0.05, dataMatriz)
+np.savetxt("QroMatrix{},{}.csv".format(dateS.year,dateS.month), dataMatriz, delimiter=",")
 cmap=pl.cm.viridis
 cmap.set_bad(color='white')
 ax, cf = wrl.vis.plot_ppi(dataMatriz , cmap="viridis")
